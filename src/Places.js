8df7b72955c10e193fba31937c4d20d1f57a80ff
ayushii19 <ayushi190702@gmail.com>
2023-04-14 14:20:38 +0530

    message

--- src/Places.js
+++ src/Places.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const Places = () => {
+
+    const [ data, setData ] = useState( [] );
+
+    useEffect(() => {
+        fetch("https://api.github.com/users").then((response)=>response.json()).then((data)=>{setData( data )})
+    }, [])
+
     return(
         <div>
-            
+           { data.map( ( x ) => {
+            return <div>
+                <span>{x.id}</span>
+                <span>{x.login}</span>
+            </div>
+           } ) } 
         </div>
     )
 }
